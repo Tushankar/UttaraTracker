9ece7a3e6de767e32a11bed6278b2697a12b6c26
Tushankar Saha <sahatushankar234@gmail.com>
2026-03-08 17:04:30 +0530

    study tracker

--- server.js
+++ server.js
@@ -11,12 +11,28 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: [
+      "https://studytrackertt.netlify.app",
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
+    credentials: true,
+  },
 });
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://studytrackertt.netlify.app",
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "10mb" }));
 
 // Serve client static files
